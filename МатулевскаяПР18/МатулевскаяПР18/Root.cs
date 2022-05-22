989445074ca24c96123e496215c8df4d47e8eb2b
Кристина Матулевская <matulevskaakristina@gmail.com>
2022-05-22 23:57:34 +0300

    Добавьте файлы проекта.

---
+++ МатулевскаяПР18/МатулевскаяПР18/Root.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace МатулевскаяПР18
+{
+    
+    
+        public class Root
+        {
+            public string activity { get; set; }
+            public string type { get; set; }
+            public double participants { get; set; }
+            public double price { get; set; }
+            public string link { get; set; }
+            public string key { get; set; }
+            public double accessibility { get; set; }
+        }
+    
+}
